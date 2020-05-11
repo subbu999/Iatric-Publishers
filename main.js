642f7175fbf3916d4bc4c183593f6b12c1c3819b
subbu999 <b.subbu504@gmail.com>
2020-05-11 13:58:46 +0530

    Instructins for authors added

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(()=>{
     new WOW().init();
+    $(window).scroll(()=>{
+        
+        if(Math.round($(window).scrollTop())>=300){
+            $('.scroll-top').css({'display':'block'}).fadeIn(800);
+        }
+    })
+    $('.scroll-top a').on("click",function(e){
+       if(this.hash !== ''){
+           const hash = this.hash;
+           e.preventDefault();
+           $('html, body').animate({
+              scrollTop:$(hash).offset().top
+           },800,function(){
+               window.location.hash = hash;
+           });
+       }
+    })
 })
